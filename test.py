cca6abf85e9705adac8981ac7ba709e5f586b982
BazzS <bazz91@mail.ru>
2021-06-24 14:11:13 +0300

    tests with allure

--- test.py
+++ test.py
@@ -12,6 +12,8 @@ from .verify_text import SearchVerifyText
 import time
 import unittest
 from selenium import webdriver
+import pytest
+import allure
 
 class PlayGround(unittest.TestCase):
     def setUp(self):
@@ -19,89 +21,97 @@ class PlayGround(unittest.TestCase):
         self.driver.implicitly_wait(5)
 
     def test_classattr(self):
-        resource = "classattr"
-        browser = self.driver
-        page = SearchHelpClassAttr(browser)
-        page.go_to_site(resource)
-
-        page.click_button()
-        page.switch_to_alert_accept()
-        button = page.test_button()
-        self.assertTrue(button)
+        with allure.step("Launch site"):
+            resource = "classattr"
+            browser = self.driver
+            page = SearchHelpClassAttr(browser)
+            page.go_to_site(resource)
+        with allure.step("Switch to alert accept"):
+            page.click_button()
+            page.switch_to_alert_accept()
+            button = page.test_button()
+            self.assertTrue(button)
 
     def test_click(self):
-        resource = "click"
-        browser = self.driver
-        page = SearchClick(browser)
-        page.go_to_site(resource)
+        with allure.step("Launch site"):
+            resource = "click"
+            browser = self.driver
+            page = SearchClick(browser)
+            page.go_to_site(resource)
+        with allure.step("Click button"):
+            page.click_button()
+            button = page.click_button()
+            self.assertTrue(button)
 
-        page.click_button()
-        button = page.click_button()
-        self.assertTrue(button)
+    def test_clientdelay(self):
+        with allure.step("Launch site"):
+            resource = "clientdelay"
+            browser = self.driver
+            page = SearchHelpClientDelay(browser)
+            page.go_to_site(resource)
+        with allure.step("Wait to text"):
+            page.click_button()
+            button = page.wait_to_text()
+            self.assertTrue(button)
 
-    # def test_clientdelay(self):
-    #     resource = "clientdelay"
-    #     browser = self.driver
-    #     page = SearchHelpClientDelay(browser)
-    #     page.go_to_site(resource)
-    #
-    #     page.click_button()
-    #     button = page.wait_to_text()
-    #     self.assertTrue(button)
+    def test_dinamictable(self):
+        with allure.step("Launch site"):
+            resource = "dynamictable"
+            browser = self.driver
+            page = SearchDynamicTable(browser)
+            page.go_to_site(resource)
+        with allure.step("Chrome CPU"):
+            chrome_cpu = page.chrome_cpu()
+            tasks = page.tasks()
+            list_task = tasks.text.split("\n")
+            chrome_task = [chrome for chrome in list_task if chrome.startswith("Chrome")]
+            chrome_detail_list = chrome_task[0].split()
+            cpu = [cpu for cpu in chrome_detail_list if '%' in cpu]
+            self.assertEqual(chrome_cpu, f'{chrome_detail_list[0]} CPU: {cpu[0]}')
 
-    # def test_dinamictable(self):
-    #     resource = "dynamictable"
-    #     browser = self.driver
-    #     page = SearchDynamicTable(browser)
-    #     page.go_to_site(resource)
-    #
-    #     chrome_cpu = page.chrome_cpu()
-    #     tasks = page.tasks()
-    #     list_task = tasks.text.split("\n")
-    #     chrome_task = [chrome for chrome in list_task if chrome.startswith("Chrome")]
-    #     chrome_detail_list = chrome_task[0].split()
-    #     cpu = [cpu for cpu in chrome_detail_list if '%' in cpu]
-    #     self.assertEqual(chrome_cpu, f'{chrome_detail_list[0]} CPU: {cpu[0]}')
-
-    # def test_hiddenlayers(self):
-    #     resource = "hiddenlayers"
-    #     browser = self.driver
-    #     page = SearchHiddenLayers(browser)
-    #     page.go_to_site(resource)
-    #
-    #     first = page.click_button()
-    #     self.assertTrue(first)
-    #     second = page.click_button()
-    #     self.assertFalse(second)
+    def test_hiddenlayers(self):
+        with allure.step("Launch site"):
+            resource = "hiddenlayers"
+            browser = self.driver
+            page = SearchHiddenLayers(browser)
+            page.go_to_site(resource)
+        with allure.step("True click"):
+            first = page.click_button()
+            self.assertTrue(first)
+            second = page.click_button()
+            self.assertFalse(second)
 
     def test_mouseover(self):
-        resource = "mouseover"
-        browser = self.driver
-        page = SearchMouseOver(browser)
-        page.go_to_site(resource)
-
-        first_count = page.find_click_count()
-        page.double_click_link()
-        second_count = page.find_click_count()
-        self.assertEqual(int(second_count) - int(first_count), 2)
+        with allure.step("Launch site"):
+            resource = "mouseover"
+            browser = self.driver
+            page = SearchMouseOver(browser)
+            page.go_to_site(resource)
+        with allure.step("Click count"):
+            first_count = page.find_click_count()
+            page.double_click_link()
+            second_count = page.find_click_count()
+            self.assertEqual(int(second_count) - int(first_count), 2)
 
     def test_sampleapp(self):
-        resource = "sampleapp"
-        browser = self.driver
-        page = SearchSampleApp(browser)
-        page.go_to_site(resource)
-
-        login_status = page.login_status()
-        self.assertEqual(login_status, 'User logged out.')
+        with allure.step("Launch site"):
+            resource = "sampleapp"
+            browser = self.driver
+            page = SearchSampleApp(browser)
+            page.go_to_site(resource)
+        with allure.step("Login status of page"):
+            login_status = page.login_status()
+            self.assertEqual(login_status, 'User logged out.')
 
     def test_verifytext(self):
-        resource = "verifytext"
-        browser = self.driver
-        page = SearchVerifyText(browser)
-        page.go_to_site(resource)
-
-        text = page.search_text()
-        self.assertEqual(text, "Welcome UserName!")
+        with allure.step("Launch site"):
+            resource = "verifytext"
+            browser = self.driver
+            page = SearchVerifyText(browser)
+            page.go_to_site(resource)
+        with allure.step("Search text"):
+            text = page.search_text()
+            self.assertEqual(text, "Welcome UserName!")
 
     def tearDown(self):
         self.driver.quit()
